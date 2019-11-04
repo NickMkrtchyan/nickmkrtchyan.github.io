667c5bff730e8a5e34dba25726efc1f9929bf481
Nick Mkrtchyan <nickmkrtchyan@gmail.com>
2019-11-04 16:46:24 +0300

    .

--- null/js/ajax-mail.js
+++ null/js/ajax-mail.js
@@ -2,34 +2,11 @@
 	"use strict";
 
     // form validation
-    $('#contact-form .input_field.required').on('change paste blur', function () {
+    $('#contact-form .input_field.required').on('blur', function () {
 		if ($(this).val().trim() == '') { $(this).addClass('inputEmpty').removeClass('inputNotEmpty'); }
 		else { $(this).removeClass('inputEmpty').addClass('inputNotEmpty'); }
 		if ($(this).val().trim() == '') { $(this).attr('placeholder', 'Field is Required!'); }
     });
-	
-    $("input[type='email']").on('change paste blur', function () {
-        var sEmail = $("input[type='email']").val();
-        if ($.trim(sEmail).length == 0) {
-            $(this).addClass('invalid').removeClass('valid');
-        }
-        if (validateEmail(sEmail)) {
-            $(this).addClass('valid').removeClass('invalid');
-        }
-        else {
-            $(this).addClass('invalid').removeClass('valid');
-        }
-    });
-
-    function validateEmail(sEmail) {
-        var filter = /^([\w-\.]+)@((\[[0-9]{1,10}\.[0-9]{1,10}\.[0-9]{1,10}\.)|(([\w-]+\.)+))([a-zA-Z]{2,10}|[0-9]{1,10})(\]?)$/;
-        if (filter.test(sEmail)) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 	
 	var form = $('#contact-form');
 	var formMessages = $('.form-message');
